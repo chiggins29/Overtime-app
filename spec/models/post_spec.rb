3b3c242513e23f1407fb6e9f2b37b0c2831d1c19
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-22 14:21:55 -0600

    added twitter bootstrap

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "Creation" do
-  	before do  
-  		@post = Post.create(date: Date.today, rationale: "Anything")
-  	end
+    before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Smith")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: user.id)
+    end
 
-  	it "can be created" do
-  		expect(@post).to be_valid
-  	end
-
-
-  	it "cannot be created without a date and rationale" do
-  		@post.date = nil 
-  		@post.rationale = nil 
-  		expect(@post).to_not be_valid
-  	end
+    it "can be created" do
+      
+      expect(@post).to be_valid
+    end
+    it "cannot be created without datee, rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
   end
 end
