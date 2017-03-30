64efc9b8b35a112341fc3d0df2a4387c3cf6f5aa
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-30 15:25:39 -0600

    added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 puts "1 User created"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rantionale content", user_id: @user.id)
+	Post.create!(date: Date.today, rationale: "#{post} rantionale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
