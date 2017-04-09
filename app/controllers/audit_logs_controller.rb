40ddfd2ba65ebba1a0a7b3c3985ecb22f5013c68
Cody Higgins <cody.higginsdev18@gmail.com>
2017-04-09 14:13:40 -0600

    added new clone

--- app/controllers/audit_logs_controller.rb
+++
@@ -1,6 +0,0 @@
-class AuditLogsController < ApplicationController
-  def index
-    @audit_logs = AuditLog.page(params[:page]).per(10)
-    authorize @audit_logs
-  end
-end
--- db/migrate/20170330203826_add_post_hour_request_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddPostHourRequestToPosts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :posts, :overtime_request, :decimal, default: 0.0
-  end
-end
